a083f5a9582e51f87ce3237f33ee9c80e4bb4480
Julia <nai.julia@gmail.com>
2021-02-18 22:05:47 -0800

    add dict method to match, player, and summ class

--- parsing.py
+++ parsing.py
@@ -4,16 +4,16 @@ import re
 import html
 
 class Summoner:
-    def __init__(self, summoner_name):
+    def __init__(self, summoner_name: str):
         self.name = summoner_name
 
-        self.id = None
+        self.summoner_id = None
         self.url = None
         self.rank = None
         self.id_url_rank()
 
     def __str__(self):
-        output = f"{self.name} {self.rank} {self.id}"
+        output = f"{self.name} {self.rank} {self.summoner_id}"
         return output
 
     def id_url_rank(self):
@@ -25,28 +25,41 @@ class Summoner:
         summ_id = child_div['data-summoner-id']
 
         self.url = url
-        self.id = summ_id
+        self.summoner_id = summ_id
 
         rank = soup.find("div", {"class": "TierRank"}).string
 
         self.rank = rank
 
-    def print():
-        print(self.name, self.rank, self.id)
+    def to_dict(self) -> dict:
+        summoner = {
+            "name": self.name,
+            "summoner_id": self.summoner_id,
+            "url": self.url,
+            "rank": self.rank,
+        }
+        return summoner
 
 class Player:
     def __init__(self, champion: str, summoner: Summoner):
-        self.summoner = summoner
         self.champion = champion
+        self.summoner = summoner
 
     def __str__(self):
-        output = f"{self.champion} {self.summoner}"  # TODO: This isn't working when line 55 tries to print(i)
+        output = f"{self.champion} {self.summoner}"
         return output
+
+    def to_dict(self) -> dict:
+        player = {
+            "champion": self.champion,
+            "summoner": self.summoner.to_dict(),
+        }
+        return player
 
 class Match:
     def __init__(self):
         self.teams = []
-        self.id = None
+        self.match_id = None
 
     def add_team(self, team):
         self.teams.append(team)
@@ -54,6 +67,16 @@ class Match:
     def print_team(self, team_num):
         for i in self.teams[team_num]:
             print(i)
+
+    def to_dict(self) -> dict:
+        match = {
+            "match_id": self.match_id,
+            "teams": [],
+        }
+        for team in teams:
+            match["teams"].append(team.to_dict())
+
+        return match
 
 def summ_id(summ_name):
     url = 'https://na.op.gg/summoner/userName=' + summ_name.replace(' ','+')
@@ -92,6 +115,9 @@ def get_team_data(summoner_name):
     win_loss = soup.findAll("div", {"class": "GameResult"})
     teams = [a.get_text() for a in soup.find_all("div", {"class": "Team"})]
     return teams
+
+def persist_match(match: Match):
+    ...
 
 def two_matches(summoner_name: str):
     """
